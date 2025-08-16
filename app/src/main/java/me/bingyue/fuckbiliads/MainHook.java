8616e779ef75f2e3e6d3ec7c79c6b79bc0bf4307
bingqiu456 <bingyuevip@gmail.com>
2025-08-16 17:04:49 +0800

    End

--- app/src/main/java/me/bingyue/fuckbiliads/MainHook.java
+++ app/src/main/java/me/bingyue/fuckbiliads/MainHook.java
@@ -1,66 +1,17 @@
 package me.bingyue.fuckbiliads;
 
 
-import android.content.Context;
-import android.content.SharedPreferences;
-import android.content.pm.PackageManager;
-import android.util.Log;
-import android.widget.Toast;
-
 import de.robv.android.xposed.IXposedHookLoadPackage;
-import de.robv.android.xposed.XC_MethodHook;
+import de.robv.android.xposed.XC_MethodReplacement;
 import de.robv.android.xposed.XposedHelpers;
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
 
 
 
 public class MainHook implements IXposedHookLoadPackage {
-
-
-    static {
-        System.loadLibrary("dexkit");
-    }
-
-    private XC_MethodHook.Unhook applicationCreateUnhook;
-
     @Override
     public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
         if (!lpparam.packageName.equals("tv.danmaku.bili")) return;
-
-        applicationCreateUnhook = XposedHelpers.findAndHookMethod(
-                "android.app.Application",
-                lpparam.classLoader,
-                "onCreate",
-                new XC_MethodHook() {
-                    @Override
-                    protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-                        Context hostContext = (Context) param.thisObject;
-                        int version_code = GetBiliiliVersion.B(hostContext, lpparam.packageName);
-                        saveDataToHost(hostContext, version_code, lpparam);
-                    }
-                }
-        );
-
-
-    }
-
-    private void saveDataToHost(Context hostContext, Integer version_code_, XC_LoadPackage.LoadPackageParam lpparam) throws NoSuchMethodException, PackageManager.NameNotFoundException, ClassNotFoundException {
-        SharedPreferences prefs = hostContext.getSharedPreferences(
-                "me_bingyue_bilifuckads",
-                Context.MODE_PRIVATE
-        );
-
-        int version_code = prefs.getInt("now_version", 0);
-        if(version_code == 0){
-            Toast.makeText(hostContext, "正在初始化去广告模块", Toast.LENGTH_SHORT).show();
-
-        }
-        if(version_code != version_code_){
-            Toast.makeText(hostContext, "检测到你[升级|降级]了b站", Toast.LENGTH_SHORT).show();
-            prefs.edit().putString("BaseSplash", "").apply();
-        }
-        HookAD.dexkit_hooker(lpparam, hostContext);
-        Toast.makeText(hostContext, "模块搜索完毕，已缓存", Toast.LENGTH_SHORT).show();
-        applicationCreateUnhook.unhook();
+        XposedHelpers.findAndHookMethod("tv.danmaku.bili.ui.splash.ad.model.Splash", lpparam.classLoader, "isValid", XC_MethodReplacement.returnConstant(false));
     }
 }
