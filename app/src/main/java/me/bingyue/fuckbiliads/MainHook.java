e25b144bbf643d86e960ec6975db679ea1cbb015
thaomaitam <thaomtam@gmail.com>
2025-09-03 16:32:25 +0700

    Update MainHook.java

--- app/src/main/java/me/bingyue/fuckbiliads/MainHook.java
+++ app/src/main/java/me/bingyue/fuckbiliads/MainHook.java
@@ -11,7 +11,7 @@ import de.robv.android.xposed.callbacks.XC_LoadPackage;
 public class MainHook implements IXposedHookLoadPackage {
     @Override
     public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
-        if (!lpparam.packageName.equals("tv.danmaku.bili")) return;
-        XposedHelpers.findAndHookMethod("tv.danmaku.bili.ui.splash.ad.model.Splash", lpparam.classLoader, "isValid", XC_MethodReplacement.returnConstant(false));
+        if (!lpparam.packageName.equals("com.bstar.intl")) return;
+        XposedHelpers.findAndHookMethod("com.bstar.intl.ui.splash.ad.model.Splash", lpparam.classLoader, "isValid", XC_MethodReplacement.returnConstant(false));
     }
 }
